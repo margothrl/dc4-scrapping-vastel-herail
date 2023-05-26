bdbec25ed844a367b4e28abda62005be0c5c0723
Margot Herail <margot.herail@hotmail.fr>
2023-05-26 15:16:55 +0200

    ÉTAPE 2 : parcourir 10 pages du site + header csv

--- fichier.py
+++ fichier.py
@@ -8,3 +8,16 @@ import csv
 result_file = open("result.csv", "w", encoding="utf-8")
 writer = csv.writer(result_file, delimiter=",")
 header = ["Team", "Country", "Year", "Wins", "Losses", "Draws", "Goals For (GF)", "Goals Against (GA)", "+/-"]
+
+writer.writerow(header)
+
+# ÉTAPE 2 : Parcourir les dix premières pages du site
+for i in range(1, 11):
+    url = f"https://www.scrapethissite.com/pages/forms/?page_num={i}"
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    rows = soup.find_all("tr", class_="team")
+
+    for row in rows[1:]:
+        data = [td.text.strip() for td in row.find_all("td")]
